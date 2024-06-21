d732f9bddc264f06b28f738b9051ae483b541ce3
Ankit Kumar <kr21ankit@gmail.com>
2024-06-21 13:31:30 +0530

    making dynamic

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ window.addEventListener('load', () => {
     preloader.remove();
 })
 
+
 // JavaScript for Tabs
-$(document).ready(function() {
+$(document).ready(function () {
     $('#myTab a').on('click', function (e) {
         e.preventDefault();
         $(this).tab('show');
@@ -13,5 +14,27 @@ $(document).ready(function() {
 });
 
 
-// Slider
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const articlesContainer = document.querySelector('.articles');
+
+//     function fetchVideoLinks() {
+//         db.collection('videoLinks').get().then((querySnapshot) => {
+//             querySnapshot.forEach((doc) => {
+//                 const videoLink = doc.data().link;
+//                 const iframe = document.createElement('iframe');
+//                 iframe.className = 'video';
+//                 iframe.src = videoLink;
+//                 iframe.title = 'YouTube video player';
+//                 iframe.frameBorder = '0';
+//                 iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+//                 iframe.allowFullscreen = true;
+//                 articlesContainer.appendChild(iframe);
+//             });
+//         }).catch((error) => {
+//             console.error('Error fetching video links:', error);
+//         });
+//     }
+
+//     fetchVideoLinks();
+// });
