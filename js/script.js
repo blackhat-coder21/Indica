b118160ef744d9125e6ee378e1640c677ab3f111
Ankit Kumar <kr21ankit@gmail.com>
2024-06-27 17:40:59 +0530

    few changes

--- js/script.js
+++ js/script.js
@@ -5,36 +5,9 @@ window.addEventListener('load', () => {
 })
 
 
-// JavaScript for Tabs
 $(document).ready(function () {
     $('#myTab a').on('click', function (e) {
         e.preventDefault();
         $(this).tab('show');
     });
 });
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const articlesContainer = document.querySelector('.articles');
-
-//     function fetchVideoLinks() {
-//         db.collection('videoLinks').get().then((querySnapshot) => {
-//             querySnapshot.forEach((doc) => {
-//                 const videoLink = doc.data().link;
-//                 const iframe = document.createElement('iframe');
-//                 iframe.className = 'video';
-//                 iframe.src = videoLink;
-//                 iframe.title = 'YouTube video player';
-//                 iframe.frameBorder = '0';
-//                 iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
-//                 iframe.allowFullscreen = true;
-//                 articlesContainer.appendChild(iframe);
-//             });
-//         }).catch((error) => {
-//             console.error('Error fetching video links:', error);
-//         });
-//     }
-
-//     fetchVideoLinks();
-// });
